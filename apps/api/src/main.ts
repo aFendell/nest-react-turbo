8ef51c2121f4f18edb9ced0ca76acd59cbee36ac
Assaf Fendell <fendell.assaf@gmail.com>
2023-11-02 14:25:15 +0200

    feat: config service read env vars

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService: ConfigService = app.get(ConfigService);
+  const PORT = configService.get('PORT');
+
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+
+  await app.listen(PORT || 3000);
 }
 bootstrap();
